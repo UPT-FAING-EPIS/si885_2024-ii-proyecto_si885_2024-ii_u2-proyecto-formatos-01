efebbeb0e568f2938334fa2c01c82c385fbbc6ea
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-11-18 09:51:06 -0500

    Update outputs.tf @AlbertApaza #19
    #19

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,24 @@
-no recuedo para que era el outputs
+output "rds_endpoint" {
+  description = "El endpoint de la instancia RDS"
+  value       = aws_db_instance.mydb_instance.endpoint
+}
+
+output "rds_port" {
+  description = "El puerto de la instancia RDS"
+  value       = aws_db_instance.mydb_instance.port
+}
+
+output "vpc_id" {
+  description = "El ID de la VPC creada"
+  value       = aws_vpc.my_vpc.id
+}
+
+output "public_subnet_id" {
+  description = "El ID de la subred pública"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "El ID de la subred privada"
+  value       = aws_subnet.private_subnet.id
+}
