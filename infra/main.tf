8228b5d257958e6329e1be4f5419933cdd6f0a7d
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-11-18 13:27:56 -0500

    Update main.tf @AlbertApaza #18
    #18

--- infra/main.tf
+++ infra/main.tf
@@ -1,56 +1,24 @@
 # Proveedor AWS
 provider "aws" {
-  region = "us-east-1"  # Cambia la región si es necesario
+  region = var.region
 }
 
-# Crear un nuevo VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.31.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "my-vpc"
-  }
+# Buscar la VPC existente
+data "aws_vpc" "lab_vpc" {
+  id = "vpc-02ee44d7135379986"
 }
 
-# Crear un Internet Gateway y asociarlo con la VPC
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my-igw"
-  }
+# Usar el grupo de subredes predeterminado de la VPC
+data "aws_db_subnet_group" "default" {
+  name = "default-vpc-02ee44d7135379986"
 }
 
-# Crear una Subnet pública
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "172.31.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet"
-  }
-}
-
-# Crear una Subnet privada
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "172.31.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-# Crear un Security Group para RDS
+# Crear el Security Group para RDS
 resource "aws_security_group" "rds_sg" {
   name        = "rds-security-group"
   description = "Security group for RDS instance"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = data.aws_vpc.lab_vpc.id
 
-  # Permitir acceso a MySQL (puerto 3306) desde cualquier IP
   ingress {
     from_port   = 3306
     to_port     = 3306
@@ -58,7 +26,6 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Permitir todo el tráfico saliente
   egress {
     from_port   = 0
     to_port     = 0
@@ -71,22 +38,14 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
-# Crear un DB Subnet Group
-resource "aws_db_subnet_group" "my_db_subnet_group" {
-  name        = "my-db-subnet-group"
-  subnet_ids  = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
-  description = "Subnet group for RDS instance"
-}
-
-# Crear la instancia de RDS MySQL
-resource "aws_db_instance" "mydb_instance" {
-  identifier            = "mydb-instance"
+resource "aws_db_instance" "labRDS" {
+  identifier            = "labrds-instance"
   engine                = "mysql"
   engine_version        = "5.7"
   instance_class        = "db.t3.micro"
   allocated_storage     = 20
   storage_type          = "gp2"
-  db_subnet_group_name  = aws_db_subnet_group.my_db_subnet_group.name
+  db_subnet_group_name  = data.aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   username              = "admin"
   password              = "MySecurePassword123!"
@@ -96,6 +55,6 @@ resource "aws_db_instance" "mydb_instance" {
   backup_retention_period = 7
   skip_final_snapshot   = true
   tags = {
-    Name = "mydb-instance"
+    Name = "labrds-instance"
   }
 }
