760176bd89eddcf8b1d355882900eaf3ebce0bd7
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-11-18 00:41:15 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,19 @@
-definir varibles
+# Definición de variables en Terraform
+
+variable "region" {
+  description = "La región de AWS en la que se desplegarán los recursos"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_class" {
+  description = "Clase de la instancia RDS"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  description = "Espacio de almacenamiento asignado a la instancia RDS"
+  type        = number
+  default     = 20
+}
