e02cbac429aea6045d0c98e814c44288adf09ed4
Jesus Antonio Huallpa Maron <102675596+jesushuallpaEPIS3@users.noreply.github.com>
2024-11-19 01:13:12 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,3 +10,15 @@ variable "region" {
   type        = string
   default     = "us-east-1"  # Puedes asignar una región predeterminada o dejarlo vacío
 }
+resource "aws_instance" "example" {
+  instance_type = var.instance_class
+  ami            = "ami-123456"
+  region         = var.region
+}
+
+resource "aws_db_instance" "example_db" {
+  instance_class = var.instance_class
+  allocated_storage = var.allocated_storage
+  region = var.region
+  engine = "mysql"
+}
