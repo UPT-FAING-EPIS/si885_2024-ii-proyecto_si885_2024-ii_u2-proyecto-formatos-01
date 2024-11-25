d74b9bb68a6a827be9d93ce1492b912f46986059
AlbertApaza <102675510+AlbertApaza@users.noreply.github.com>
2024-11-25 12:09:12 -0500

    Update s3bucket.py

--- artefactos/s3bucket.py
+++ artefactos/s3bucket.py
@@ -5,12 +5,16 @@ s3 = boto3.client('s3', region_name='us-east-1')
 
 bucket_name = 'netuptinteligencianegocios'
 folder_name = 'datosimportantes/'  # Carpeta virtual (nota el '/' al final)
+file_name = 'datos_combinados.csv'  # El nombre del archivo a subir
+file_path = './datos_combinados.csv'  # Asumiendo que el archivo está en el mismo directorio que el script
 
 def lambda_handler(event, context):
     try:
-        # Crear un objeto vacío con el nombre de la carpeta
-        s3.put_object(Bucket=bucket_name, Key=folder_name)
-        print(f'Carpeta "{folder_name}" creada con éxito en el bucket {bucket_name}.')
+        # Subir el archivo a la carpeta en S3
+        s3.upload_file(file_path, bucket_name, folder_name + file_name)
+        print(f'Archivo "{file_name}" cargado con éxito a S3://{bucket_name}/{folder_name}{file_name}.')
+    except FileNotFoundError:
+        print(f'El archivo {file_path} no fue encontrado.')
     except NoCredentialsError:
         print('No se encontraron credenciales de AWS.')
     except PartialCredentialsError:
