b54bf730534105a102024b5e5e55472da547de45
Jesus Antonio Huallpa Maron <102675596+jesushuallpaEPIS3@users.noreply.github.com>
2024-11-26 03:43:46 -0500

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     infracost = {
       source  = "infracost/infracost"
-      version = "~> 0.10.7" # Asegúrate de usar una versión válida
+      version = "~> 1.9.8" # Asegúrate de usar una versión válida
     }
   }
 }
